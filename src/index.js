18e489d6978130a5b4ad2899a550ed490da059f2
Ashutosh <ashu.heretocode@gmail.com>
2021-01-09 14:12:14 +0530

    initialized the repo by cleaning unnecessary files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
+// First two lines are absolute necessary for any react project. Index.js is the main js file. 
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './App'; // loaded our root component here
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//while rendering, we must specify the component and where to render it.
+ReactDOM.render(<App />,document.getElementById('root'));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
